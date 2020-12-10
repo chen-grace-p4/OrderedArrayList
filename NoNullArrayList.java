4a90487eedcddf35840997ba527405ab71fabe57
Grace <gchen20@stuy.edu>
2020-12-10 13:21:36 -0500

    Fixed add() to detect null, added discussion, tested add()

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,7 +6,7 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
   public boolean add(T element) {
-    if (element.equals(null)) {
+    if (element == null) {
       throw new IllegalArgumentException("Added element shouldn't be null");
     } else {
       return super.add(element);
--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester {
   public static void main(String[]args) {
     NoNullArrayList<String> hello = new NoNullArrayList<String>();
+    System.out.println(hello.add(null));
     System.out.println(hello.add("hi"));
   }
 }
