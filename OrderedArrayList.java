8bf8de0016cd19a6b65eacaa458e8e72da3dd8d3
Grace <gchen20@stuy.edu>
2020-12-13 19:34:10 -0500

    Tested and implemented a private helper method and overwrote add

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,4 +1,25 @@
 import java.util.ArrayList;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public OrderedArrayList() {
+    super();
+  }
+
+  private int findIndex(T element) {
+    for (int i = 0; i < super.size(); i++) {
+      if (element.compareTo(super.get(i)) < 0) {
+        return i;
+      }
+    }
+    return super.size();
+  }
+
+  public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Added element shouldn't be null");
+    } else {
+      super.add(findIndex(element), element);
+      return true;
+    }
+  }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 public class Tester {
   public static void main(String[]args) {
-    NoNullArrayList<String> hello = new NoNullArrayList<String>();
-    NoNullArrayList<String> hello2 = new NoNullArrayList<String>(10);
+    // NoNullArrayList<String> hello = new NoNullArrayList<String>();
+    // NoNullArrayList<String> hello2 = new NoNullArrayList<String>(10);
     // // System.out.println(hello.add(null));
     // System.out.println(hello.add("hi"));
     //
@@ -13,5 +13,22 @@ public class Tester {
     // // hello.set(1, null);
     //
     // System.out.println(hello);
+    // hello.add("hi");
+    // hello.add("bye");
+    // hello.add("boo");
+    // hello.add(3, "woah");
+    // System.out.println(hello);
+
+    OrderedArrayList<Integer> hello = new OrderedArrayList<Integer>();
+    hello.add(1);
+    hello.add(2);
+    hello.add(4);
+    System.out.println(hello);
+    hello.add(3);
+    System.out.println(hello);
+    hello.add(-5);
+    hello.add(100);
+    System.out.println(hello);
+    hello.add(null);
   }
 }
