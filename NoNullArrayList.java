b59e8611abe0b9357cd8d7c44bc33c80795ed2c4
Grace <gchen20@stuy.edu>
2020-12-13 19:00:07 -0500

    Tested and overwrote add(index,element)

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -12,4 +12,17 @@ public class NoNullArrayList<T> extends ArrayList<T> {
       return super.add(element);
     }
   }
+
+  public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Added element shouldn't be null");
+    } else {
+      super.add(index, element);
+    }
+  }
+
+  public T set(int index, T element) {
+    return element;
+  }
+
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,12 @@
 public class Tester {
   public static void main(String[]args) {
     NoNullArrayList<String> hello = new NoNullArrayList<String>();
-    System.out.println(hello.add(null));
+    // System.out.println(hello.add(null));
     System.out.println(hello.add("hi"));
+
+    // hello.add(1, null);
+    hello.add(1, "hi");
+
+    System.out.println(hello);
   }
 }
