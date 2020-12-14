95f230a0979189e3ef2b389551c2773a235b1bd1
Grace <gchen20@stuy.edu>
2020-12-13 19:04:21 -0500

    Tested and added second constructor with capacity

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -5,6 +5,10 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     super();
   }
 
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+
   public boolean add(T element) {
     if (element == null) {
       throw new IllegalArgumentException("Added element shouldn't be null");
--- Tester.java
+++ Tester.java
@@ -1,16 +1,17 @@
 public class Tester {
   public static void main(String[]args) {
     NoNullArrayList<String> hello = new NoNullArrayList<String>();
-    // System.out.println(hello.add(null));
-    System.out.println(hello.add("hi"));
-
-    // hello.add(1, null);
-    hello.add(1, "hi");
-    System.out.println(hello);
-
-    System.out.println(hello.set(0, "bye"));
-    // hello.set(1, null);
-
-    System.out.println(hello);
+    NoNullArrayList<String> hello2 = new NoNullArrayList<String>(10);
+    // // System.out.println(hello.add(null));
+    // System.out.println(hello.add("hi"));
+    //
+    // // hello.add(1, null);
+    // hello.add(1, "hi");
+    // System.out.println(hello);
+    //
+    // System.out.println(hello.set(0, "bye"));
+    // // hello.set(1, null);
+    //
+    // System.out.println(hello);
   }
 }
